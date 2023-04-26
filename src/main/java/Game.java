3593ed8276723407cf28657843c9e84d179dd237
Alena Bobrova <kaisa8721@gmail.com>
2023-04-26 13:59:10 +0300

    Second commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Game {
+
+    protected ArrayList<Player> players = new ArrayList<>();
+
+    public void register(Player player) {
+        players.add(player);
+    }
+
+    public Player[] findByName(String name) {
+        for (Player player : players) {
+            if (player.getName() == name) {
+                return new Player[]{player};
+            }
+        }
+        return null;
+    }
+
+    public int findByStrength(String name) {
+        for (Player player : players) {
+            if (player.getName() == name) {
+                return player.getStrength();
+            }
+        }
+        return 0;
+    }
+
+    public int round(String playerName1, String playerName2) {
+        if (findByName(playerName1) == null) {
+            throw new NotRegisteredException(
+                    "Element with name: " + playerName1 + " not found"
+            );
+        }
+        if (findByName(playerName2) == null) {
+            throw new NotRegisteredException(
+                    "Element with name: " + playerName2 + " not found"
+            );
+        }
+        if (findByStrength(playerName1) > findByStrength(playerName2)) {
+            return 1;
+        }
+        if (findByStrength(playerName1) < findByStrength(playerName2)) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,89 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+    Game game = new Game();
+    Player player1 = new Player(1, "Ваня", 40);
+    Player player2 = new Player(2, "Маша", 40);
+    Player player3 = new Player(3, "Катя", 20);
+
+    @Test
+    public void shouldWinOne() {
+
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+        int expected = 1;
+        int actual = game.round("Ваня", "Катя");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldWinTwo() {
+
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+        int expected = 2;
+        int actual = game.round("Катя", "Маша");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldWinNobody() {
+
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+        int expected = 0;
+        int actual = game.round("Ваня", "Маша");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void OneNoRegistered() {
+
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Миша", "Маша");
+        });
+    }
+
+    @Test
+    public void TwoNoRegistered() {
+
+        game.register(player1);
+        game.register(player2);
+        game.register(player3);
+
+
+        Assertions.assertThrows(NotRegisteredException.class, () -> {
+            game.round("Маша", "Коля");
+        });
+    }
+
+    @Test
+    public void searchFall() {
+
+        game.register(player1);
+        game.register(player3);
+
+        int expected = 0;
+        int actual = game.findByStrength("Маша");
+
+        assertEquals(expected, actual);
+    }
+}
